e2dba7b71b7a8eab3b7334118b8fb92eba9ed598
Frost <226335706+Frost-Levi@users.noreply.github.com>
2025-10-10 08:39:29 -0600

    Made the quiz show if you got it correct or not and added butten for next question

--- JS/QuestionLoader.js
+++ JS/QuestionLoader.js
@@ -3,7 +3,10 @@ const quizQuestion = document.querySelector('.quiz-question');
     const answer2 = document.getElementById('answer-2');
     const answer3 = document.getElementById('answer-3');
     const answer4 = document.getElementById('answer-4');
+    const screen = document.querySelector('.AnswerReveal');
     const questions = JSON.parse(localStorage.getItem('questions'));
+    var currentQuestionIndex = 0;
+    var score = 0;
 addEventListener('DOMContentLoaded', function() {
     if (questions && questions.length > 0) {
         displayQuestions(questions, 0);
@@ -32,16 +35,7 @@ function randomizeAnswers(question) {
         answers.splice(2, 2); // Remove last two answers for boolean type
         answer1.innerHTML = 'True';
         answer2.innerHTML = 'False';
-        if (answer1.innerHTML === question.correct_answer) {
-            answer1.dataset.correct = 'true';
-        } else {
-            answer1.dataset.correct = 'false';
-        }
-        if (answer2.innerHTML === question.correct_answer) {
-            answer2.dataset.correct = 'true';
-        } else {
-            answer2.dataset.correct = 'false';
-        }
+        AddcorrectAttribute(question);
     } else {
         answer3.style.display = 'block';
         answer4.style.display = 'block';
@@ -53,7 +47,12 @@ function randomizeAnswers(question) {
     answer2.innerHTML = answers[1];
     answer3.innerHTML = answers[2];
     answer4.innerHTML = answers[3];
-    // Decode HTML entities in correct answer for accurate comparison
+    AddcorrectAttribute(question);
+    
+}
+}
+function AddcorrectAttribute(question) {
+// Decode HTML entities in correct answer for accurate comparison
     var doc = new DOMParser().parseFromString(question.correct_answer, 'text/html');
     question.correct_answer = doc.documentElement.textContent;
 
@@ -78,33 +77,81 @@ function randomizeAnswers(question) {
         answer4.dataset.correct = 'false';
     }
 }
-}
 
 answer1.addEventListener('click', function() {
     if (answer1.dataset.correct === 'true') {
-        alert('Correct!');
+        correctAnswer(questions[currentQuestionIndex]);
     } else {
-        alert('Incorrect!');
+        incorrectAnswer(questions[currentQuestionIndex]);
     }
 });
 answer2.addEventListener('click', function() {
     if (answer2.dataset.correct === 'true') {
-        alert('Correct!');
+        correctAnswer(questions[currentQuestionIndex]);
     } else {
-        alert('Incorrect!');
+        incorrectAnswer(questions[currentQuestionIndex]);
     }
 });
 answer3.addEventListener('click', function() {
     if (answer3.dataset.correct === 'true') {
-        alert('Correct!');
+        correctAnswer(questions[currentQuestionIndex]);
     } else {
-        alert('Incorrect!');
+        incorrectAnswer(questions[currentQuestionIndex]);
     }
 });
 answer4.addEventListener('click', function() {
     if (answer4.dataset.correct === 'true') {
-        alert('Correct!');
+        correctAnswer(questions[currentQuestionIndex]);
     } else {
-        alert('Incorrect!');
+        incorrectAnswer(questions[currentQuestionIndex]);
     }
 });
+
+function correctAnswer(question) {
+    var modifier = 1;
+    var text = '';
+    if (question.type === 'multiple') {
+        modifier = 2;
+    }
+    switch(question.difficulty) {
+        case 'easy':
+            score += 5 * modifier;
+            text = 'Easy question answered correctly! +' + (5 * modifier) + ' points';
+            break;
+        case 'medium':
+            score += 10 * modifier;
+            text = 'Medium question answered correctly! +' + (10 * modifier) + ' points';
+            break;
+        case 'hard':
+            score += 15 * modifier;
+            text = 'Hard question answered correctly! +' + (15 * modifier) + ' points';
+            break;
+    }
+    console.log("Correct answer display attempt");
+    screen.innerHTML = 'Correct!<br>' + text + '<br><button onclick="nextqn()">Next Question</button>';
+    
+    screen.style.display = 'block';
+    console.log("Success");
+    
+}
+
+function incorrectAnswer(question) {
+    console.log("Incorrect answer display attempt");
+   screen.innerHTML = 'Incorrect!<br> The answer was: ' + question.correct_answer + '<br><button onclick="nextqn()">Next Question</button>';
+   screen.style.display = 'block';
+   console.log("Success");
+}
+function nextqn() {
+    if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+        displayQuestions(questions, currentQuestionIndex);
+        screen.style.display = 'none';
+    } else {
+        localStorage.setItem('latestScore', score);
+        const topScore = localStorage.getItem('TopScore');
+        if (score > topScore) {
+            localStorage.setItem('TopScore', score);
+        }
+        window.location.href = 'Index.html';
+    }
+}
